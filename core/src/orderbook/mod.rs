75b6c3ff15e10e3063fb07eb89418238bbf03525
Sho Kaneko <kanekoshoyu@gmail.com>
2024-08-28 19:22:42 +0800

    implement core::Orderbook struct with BST and OrderedFloat

--- core/src/orderbook/mod.rs
+++ core/src/orderbook/mod.rs
@@ -1,24 +1,26 @@
-use std::collections::HashMap;
-use guilder_abstraction::GetMarketData;
+use guilder_abstraction::Orderbook as RawOrderBook;
+use ordered_float::OrderedFloat;
+use std::collections::BTreeMap;
 
-// TODO: decide whether to use f64 or decimal
-// TODO: decide whether to use dashmap or hashma
+// TODO implement code
 pub struct Orderbook {
-    ask: HashMap<f64, f64>,
-    bid: HashMap<f64, f64>,
+    asks: BTreeMap<OrderedFloat<f64>, f64>,
+    bids: BTreeMap<OrderedFloat<f64>, f64>,
 }
-impl Orderbook {
-    pub fn new() -> Self {
-        Orderbook {
-            ask: HashMap::new(),
-            bid: HashMap::new(),
-        }
-    }
-
-    pub fn init(client: impl GetMarketData) -> Self {
-        let orderbook = Orderbook::new();
-        // TODO fill in the data, shall we use the orderbook API?
-        // client.get_price(symbol);
-        orderbook
+impl From<RawOrderBook> for Orderbook {
+    fn from(raw: RawOrderBook) -> Self {
+        let asks = raw
+            .asks
+            .clone()
+            .into_iter()
+            .map(|(key, value)| (key.into(), value))
+            .collect();
+        let bids = raw
+            .bids
+            .clone()
+            .into_iter()
+            .map(|(key, value)| (key.into(), value))
+            .collect();
+        Orderbook { asks, bids }
     }
 }
